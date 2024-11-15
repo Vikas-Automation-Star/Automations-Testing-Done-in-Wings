e6f0119788dcc4cd54f03e0010e093999a1dfd8f
Vikas <vikas.empuluri@wingsInfo.net>
2024-11-15 17:33:27 +0530

    [vikas]

---
+++ src/main/java/com/wings/pages/PurchaseOrderBaseClass.java
@@ -0,0 +1,43 @@
+package com.wings.pages;
+
+import io.appium.java_client.windows.WindowsDriver;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
+public class PurchaseOrderBaseClass extends Transaction{
+    protected String dataFile;
+    public PurchaseOrderBaseClass(WindowsDriver driver,String file){
+        super(driver);
+        dataFile=file;
+    }
+    public void voucherType() throws IOException, ParseException {
+        common.clickElement("xpath","//Edit[@Name='Voucher Type']");
+        super.selectOptionalMaster(common.getDataEvenNoKeyPresent(dataFile,"voucher"),"xpath","//Edit[@Name='Voucher Type']" );
+    }
+    public void branchSelection() throws IOException, ParseException {
+        common.clickElement("xpath", "//Edit[@Name='Branch *']");
+        super.selectMasterWithValidation(common.getData(dataFile,"branch"),"xpath", "//Edit[@Name='Branch *']");
+    }
+    public void location() throws IOException, ParseException {
+        common.clickElement("xpath", "//Edit[@Name='Location *']");
+        selectOptionalMaster(common.getData(dataFile,"location"), "xpath", "//Edit[@Name='Location *']");
+    }
+    public void chooseCurrency() throws IOException, ParseException {
+        common.clickElement("xpath", "//Edit[@Name='Trans Currency *']");
+        super.selectMasterWithValidation(common.getData(dataFile,"currency"),"xpath", "//Edit[@Name='Trans Currency *']");
+    }
+    public void partyCode() throws IOException, ParseException {
+        common.clickElement("xpath", "//Edit[@Name='Party Code']");
+        super.selectMasterWithValidation(common.getData(dataFile,"partyCode"),"xpath", "//Edit[@Name='Party Code']");
+    }
+    public void executive() throws IOException, ParseException {
+        common.clickElement("xpath","//Edit[@Name='Executive *']");
+        super.selectMasterWithValidation(common.getData(dataFile,"executive"),"xpath", "//Edit[@Name='Executive *']");
+    }
+    public void remarks() throws IOException, ParseException {
+        common.clickElement("xpath","//Edit[@Name='Remarks']");
+        super.selectOptionalMaster(common.getDataEvenNoKeyPresent(dataFile,"remarks"), "xpath","//Edit[@Name='Remarks']");
+    }
+
+}
---
+++ src/main/java/com/wings/utils/XMLUtil.java
@@ -0,0 +1,206 @@
+package com.wings.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+public class XMLUtil {
+    static Path toFile= Paths.get("mailTemplates/to.html");
+    String os = System.getProperty("os.name");
+    private String testCaseId;
+    private String description;
+    String executionMachine;
+    Document document;
+    String testCaseName="";
+
+    public static String testDraft(String testCaseId, String scriptName, String description, String status, String duration) {
+        // Generates an HTML row for each test case with its ID, script name, description, status, and duration
+        return "<tr>\n" +
+                "    <td align=\"left\" valign=\"middle\"\n" +
+                "        style=\"border: 1px solid #b6b6b6; font: normal 13px 'Segoe UI', Arial, Helvetica, sans-serif;\">" + testCaseId + "</td>\n" +
+                "    <td align=\"left\" valign=\"middle\"\n" +
+                "        style=\"border: 1px solid #b6b6b6; font: normal 13px 'Segoe UI', Arial, Helvetica, sans-serif;\">" + scriptName + "</td>\n" +
+                "    <td align=\"left\" valign=\"middle\"\n" +
+                "        style=\"border: 1px solid #b6b6b6; font: normal 13px 'Segoe UI', Arial, Helvetica, sans-serif;\">" + description + "</td>\n" +
+                "    <td align=\"left\" valign=\"middle\"\n" +
+                "        style=\"border: 1px solid #b6b6b6; font: normal 13px 'Segoe UI', Arial, Helvetica, sans-serif;\">" + status + "</td>\n" +
+                "    <td align=\"left\" valign=\"middle\"\n" +
+                "        style=\"border: 1px solid #b6b6b6; font: normal 13px 'Segoe UI', Arial, Helvetica, sans-serif;\">" + duration + "</td>\n" +
+                "</tr>\n";
+    }
+
+    public void readXMLFile(String filePath) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        document = builder.parse(new File(filePath));
+        document.getDocumentElement().normalize();
+    }
+
+    public void readTestNG(String testNGPath) throws IOException, ParserConfigurationException, SAXException {
+        NodeList suiteName = document.getElementsByTagName("test");
+
+        for (int i = 0; i < suiteName.getLength(); i++) {
+            Element suiteElement = (Element) suiteName.item(i); // Correcting loop condition
+            // Extract the attributes
+            testCaseId = suiteElement.getAttribute("testID");
+            description = suiteElement.getAttribute("description");
+
+            // You can also print them here if needed
+            System.out.println("Test Case ID: " + testCaseId);
+            System.out.println("Description: " + description);
+            readTestNGResults("target/surefire-reports/testng-results.xml");
+        }
+    }
+
+    public void readTestNGResults (String testNGResultsPath) {
+        try {
+            readXMLFile(testNGResultsPath);
+            NodeList suiteList = document.getElementsByTagName("suite");
+
+            String suiteName = "";
+            String executionStartTime = "";
+            executionMachine = InetAddress.getLocalHost().getHostName();
+            String executionEndTime = "";
+            long executionTimeMillis = 0;
+
+            if (suiteList.getLength() > 0) {
+                Element suiteElement = (Element) suiteList.item(0);
+                suiteName = suiteElement.getAttribute("name");
+                executionStartTime = suiteElement.getAttribute("started-at");
+                executionEndTime = suiteElement.getAttribute("finished-at");
+
+                // Calculate total execution time in milliseconds
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+                Date start = sdf.parse(executionStartTime);
+                Date end = sdf.parse(executionEndTime);
+                executionTimeMillis = end.getTime() - start.getTime();
+            }
+
+            // Retrieve test pass/fail information
+            int passedTests = 0;
+            int failedTests = 0;
+            int skippedTests = 0;
+            int totalTests = 0;
+            double passRate = 0;
+
+            StringBuilder testDetails = new StringBuilder();
+            NodeList testList = document.getElementsByTagName("test");
+
+            for (int i = 0; i < testList.getLength(); i++) {
+                Element testElement = (Element) testList.item(i);
+                String testCaseName = testElement.getAttribute("name");
+                double duration = Double.parseDouble(testElement.getAttribute("duration-ms")) / 1000;
+
+                NodeList testMethodTags = testElement.getElementsByTagName("test-method");
+                int middleIndex = testMethodTags.getLength() / 2;
+                if (testMethodTags.getLength() > 0) {
+                    Element testMethodElement = (Element) testMethodTags.item(middleIndex);
+                    String status = testMethodElement.getAttribute("status");
+
+                    // Update counters based on status
+                    switch (status) {
+                        case "PASS":
+                            passedTests++;
+                            break;
+                        case "FAIL":
+                            failedTests++;
+                            break;
+                        case "SKIP":
+                            skippedTests++;
+                            break;
+                    }
+
+                    // Generate and accumulate test rows
+                    // Here we are using the testCaseId and description that were set in the readTestNG method
+                    testDetails.append(testDraft(testCaseId, suiteName, description, status, String.format("%.2f s", duration)));
+                }
+            }
+            totalTests = passedTests + failedTests + skippedTests;
+            passRate = (totalTests > 0) ? (passedTests / (double) totalTests) * 100 : 0;
+
+            String jsonContent = new String(Files.readAllBytes(Paths.get("./src/main/resources/XMLdata.json")));
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, Object> jsonMap = objectMapper.readValue(jsonContent, Map.class);
+
+            // Format execution time
+            String executionTimeFormatted = String.format("%02d:%02d:%02d", (executionTimeMillis / (1000 * 60 * 60)) % 24, (executionTimeMillis / (1000 * 60)) % 60, (executionTimeMillis / 1000) % 60);
+
+            // Load HTML template(like base template)
+            String htmlTemplate = new String(Files.readAllBytes(Paths.get("mailTemplates/executiontemplate.html")), "UTF-8");
+
+            // Replace placeholders in HTML template
+            htmlTemplate = htmlTemplate.replaceAll(Pattern.quote("#ExecutionMachine#"), executionMachine);
+            htmlTemplate = htmlTemplate.replaceAll(Pattern.quote("#SuiteName#"), suiteName);
+            htmlTemplate = htmlTemplate.replaceAll(Pattern.quote("#TestPassRate#"), String.format("%.2f%%", passRate));
+            htmlTemplate = htmlTemplate.replaceAll(Pattern.quote("#PassedTests#"), Integer.toString(passedTests));
+            htmlTemplate = htmlTemplate.replaceAll(Pattern.quote("#FailedTests#"), Integer.toString(failedTests));
+            htmlTemplate = htmlTemplate.replaceAll(Pattern.quote("#SkippedTests#"), Integer.toString(skippedTests));
+            htmlTemplate = htmlTemplate.replaceAll(Pattern.quote("#ExecutionStartTime#"), executionStartTime);
+            htmlTemplate = htmlTemplate.replaceAll(Pattern.quote("#ExecutionEndTime#"), executionEndTime);
+            htmlTemplate = htmlTemplate.replaceAll(Pattern.quote("#ExecutionTime#"), executionTimeFormatted);
+            htmlTemplate = htmlTemplate.replaceAll(Pattern.quote("#TotalTests#"), String.valueOf(totalTests));
+            htmlTemplate = htmlTemplate.replaceAll(Pattern.quote("#OS#"), os);
+            htmlTemplate = htmlTemplate.replaceAll(Pattern.quote("#URL#"), jsonMap.get("app").toString());
+            htmlTemplate = htmlTemplate.replaceAll(Pattern.quote("#CompanyDetails#"),jsonMap.get("companyName").toString());
+            htmlTemplate = htmlTemplate.replaceAll(Pattern.quote("#row#"), testDetails.toString());
+
+            // Write output to HTML file
+            Files.write(toFile, htmlTemplate.getBytes("UTF-8"));
+
+            final String fromEmail = "QA@wingsinfo.net"; //requires valid gmail id
+            final String password = "TestAutomation@19"; // correct password for gmail id
+            final String toEmail = "vikas.empuluri@wingsinfo.net"; // can be any email id
+
+            System.out.println("TLSEmail Start");
+            Properties props = new Properties();
+            props.put("mail.smtp.host", "smtp.office365.com"); //SMTP Host
+            props.put("mail.smtp.port", "587"); //TLS Port
+            props.put("mail.smtp.auth", "true"); //enable authentication
+            props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+
+            //create Authenticator object to pass in Session.getInstance argument
+            Authenticator auth = new Authenticator() {
+                //override the getPasswordAuthentication method
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(fromEmail, password);
+                }
+            };
+            Session session = Session.getInstance(props, auth);
+            EmailUtil.sendEmail(session, toEmail, String.valueOf(toFile));
+
+            System.out.println("Report generated successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+            public static void main(String args[]) throws IOException, ParserConfigurationException, SAXException {
+                XMLUtil xmlUtil = new XMLUtil();
+                xmlUtil.readTestNG("TestNG/sampleSuite.xml");
+//        xmlUtil.readTestNGResults("target/surefire-reports/testng-results.xml");
+            }
+        }
+
+
