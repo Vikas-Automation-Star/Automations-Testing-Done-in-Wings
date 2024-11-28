16dcaa286abb47513ebb9e505c1a4f2d3a4c8510
Manoj <manoj.c@wingsinfo.net>
2024-11-28 08:04:41 +0530

    Batch file changes

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ ECHO ON
 for /f %%i in ("%0") do set curpath=%~dp0
 cd /d %curpath%
 
-REM Delete the allure-results directory if it exists
+
 if exist allure-results (
     rmdir /s /q allure-results
 )
