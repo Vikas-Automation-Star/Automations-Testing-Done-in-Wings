a8e0374cd1787b5819c9bc4531524cd68d055a47
Manoj <manoj.c@wingsinfo.net>
2024-12-08 17:15:41 +0530

    [Manoj] Added string utils and updated gitignore to restrict allure-results check-in

---
+++ src/main/java/com/wings/utils/StringUtil.java
@@ -0,0 +1,25 @@
+package com.wings.utils;
+
+public class StringUtil {
+
+    public static double extractNumber(String input) {
+
+        String regex = "\\d+(\\.\\d+)?"; // Matches integers or decimals
+        java.util.regex.Pattern pattern = java.util.regex.Pattern.compile(regex);
+        java.util.regex.Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find()) {
+            String number = matcher.group(); // Extract the matched number
+            return Double.parseDouble(number); // Convert to double
+        }
+
+        // Return -1 if no number is found(implementation below in calling class)
+        //        if (extractedNumber != -1) {
+        //            System.out.println("Extracted number: " + extractedNumber);
+        //        } else {
+        //            System.out.println("No number found in the input string.");
+        //        }
+        return -1;
+    }
+
+}
