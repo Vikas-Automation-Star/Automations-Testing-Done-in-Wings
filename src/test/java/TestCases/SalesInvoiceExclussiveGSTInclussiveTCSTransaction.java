2c4b88e7de68fecadd6eadaf31dcae7cdb847285
Vikas <vikas.empuluri@wingsInfo.net>
2024-12-10 14:05:00 +0530

    [vikas] copy and detach Test Cases

---
+++ src/test/java/TestCases/SalesInvoiceExclussiveGSTInclussiveTCSTransaction.java
@@ -0,0 +1,37 @@
+package TestCases;
+
+import com.wings.pages.AppLogin;
+import io.appium.java_client.windows.WindowsDriver;
+import io.qameta.allure.Allure;
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.io.IOException;
+
+public class SalesInvoiceExclussiveGSTInclussiveTCSTransaction {
+    WindowsDriver driver;
+    AppLogin appLogin = new AppLogin();
+    String dataFile = "./src/main/resources/TestCasesData/salesInvoicesExclussiveGSTAndInclussiveTCS.json";
+
+    @BeforeTest
+    public void beforeTest() throws IOException, InterruptedException, ParseException {
+        driver = appLogin.launchSingleUserApp();
+        appLogin.singleUserLogin();
+        Allure.step("Before Test TestCase01");
+    }
+
+    @Test
+    public void testcase1() throws IOException, ParseException, InterruptedException, AWTException {
+        SalesInvoiceExclussiveGstAndInclusiveTCS si=new SalesInvoiceExclussiveGstAndInclusiveTCS(driver,dataFile);
+        si.salesInvoiceExclussiveGstAndInclusiveTcs();
+    }
+
+    @AfterTest
+    public void afterTest(){
+//        appLogin.logout();
+        Allure.step("After Test TestCase 01");
+    }
+}
