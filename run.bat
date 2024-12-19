e3a771ac02017797869af747e4a414fef4182507
Vikas <vikas.empuluri@wingsInfo.net>
2024-12-19 14:56:11 +0530

    [vikas] adding few issues resolved testCases

--- run.bat
+++ run.bat
@@ -2,10 +2,11 @@ ECHO ON
 for /f %%i in ("%0") do set curpath=%~dp0
 cd /d %curpath%
 
-
+ 
 if exist allure-results (
     rmdir /s /q allure-results
 )
-
+ 
 call mvn clean install -DTestNGFile=TestNG/Regression.xml
 call mvn compile exec:java
+ 
