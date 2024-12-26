e18994790111568527d45ad6a78b866f4bcc2c86
Madhuri M <madhurimatta@wingsinfo.net>
2024-12-26 17:10:49 +0530

    [Madhuri] Updated TestCases

--- src/test/java/menuItems/sales/transactions/SalesEnquiryTransaction.java
+++ src/test/java/menuItems/sales/transactions/SalesEnquiryTransaction.java
@@ -1,6 +1,5 @@
 package menuItems.sales.transactions;
 
-import com.wings.utils.SalesEnquiriesDemo;
 import io.appium.java_client.windows.WindowsDriver;
 import io.qameta.allure.Allure;
 import org.json.simple.parser.ParseException;
@@ -9,13 +8,12 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import com.wings.pages.AppLogin;
 import com.wings.pages.sales.transactions.SalesEnquiry;
-
 import java.io.IOException;
 
 public class SalesEnquiryTransaction {
     WindowsDriver driver;
     AppLogin appLogin = new AppLogin();
-    String file = "./src/main/resources/menuItems/purchase/transactions/salesDemo.json";
+    String file = "./src/main/resources/menuItems/Sales/Transactions/salesEnquiry.json";
 
     @BeforeTest
     public void beforeTest() throws IOException, InterruptedException, ParseException {
@@ -26,15 +24,13 @@ public class SalesEnquiryTransaction {
 
     @Test
     public void SalesEnquiryTransaction() throws IOException, InterruptedException, ParseException {
-//        SalesEnquiry sales=new SalesEnquiry(driver,file);
-//        sales.salesEnquiry();
-        SalesEnquiriesDemo salesEnquiriesDemo = new SalesEnquiriesDemo(driver,file);
-        salesEnquiriesDemo.salesEnquiresDemo();
+        SalesEnquiry sales=new SalesEnquiry(driver,file);
+        sales.salesEnquiry();
     }
 
     @AfterTest
     public void afterTest(){
-//        appLogin.logout();
+        appLogin.logout();
         Allure.step("After Test Sales Enquiry");
     }
 }
