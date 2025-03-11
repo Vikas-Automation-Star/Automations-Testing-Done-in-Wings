181dc1f4813ac12d1fb5f88fd6e5774d254ab381
Vikas <vikas.empuluri@wingsInfo.net>
2025-03-11 11:57:48 +0530

    [vikas] Voucher Series and Types, Controlling Feature Management,Updated SalesInvoices tesCases Json data and XMLUtil class

--- run.bat
+++ run.bat
@@ -1,12 +1,2 @@
-ECHO OFF
-for /f %%i in ("%0") do set curpath=%~dp0
-cd /d %curpath%
-
- 
-if exist allure-results (
-    rmdir /s /q allure-results
-)
- 
-mvn clean install "-DTestNGFile=TestNG/MenuItems/regressionNew.xml"
+mvn clean install "-DTestNGFile=TestNG/MenuItems/salesInvoicesTestCases.xml"
 call mvn compile exec:java
- 
