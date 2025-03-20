92e6d8898ee4b6e2e328b4b348534dbea5cfc70b
Madhuri M <madhurimatta@wingsinfo.net>
2025-03-20 16:41:11 +0530

    [Madhuri] Masters modification

--- src/main/java/com/wings/pages/Masters.java
+++ src/main/java/com/wings/pages/Masters.java
@@ -30,16 +30,6 @@ public class Masters {
         }
     }
 
-    public void verifyMaster(String locatorType, String locator, String existingValue) {
-        WebElement element = common.findWebElement(locatorType, locator);
-        if (element.getText().equals(existingValue)) {
-            System.out.println("successfully selected/opened:- " + element.getText());
-
-        } else {
-            Assert.fail(element.getText() + "is not seleced");
-        }
-    }
-
     public void selectDropDownMaster(String element) throws InterruptedException {
         List<WebElement> elementList = common.findWebElements("xpath", "//Table/*[@Name='Data Panel']/*/*[contains(@Name,'MasterName row')]");
         System.out.println("Size :" + elementList.size());
@@ -52,34 +42,11 @@ public class Masters {
         }
     }
 
-    public void clickMaster(String item) {
-        List<WebElement> elementList = common.findWebElements("xpath", "//Table[@Name='Company Currency']/*[@Name='Data Panel']/ListItem[contains(@Name,'Row')]");
-        System.out.println("Size :" + elementList.size());
-        for (WebElement i : elementList) {
-            System.out.println(i.getText());
-            if (i.getText().contains(item)) {
-                i.click();
-                i.sendKeys(Keys.TAB, Keys.SPACE);
-            }
-        }
-    }
-
     public void actionsMaster(String locatorType, String itemsLocator, String masterLocator) {
         WebElement Allitems = common.findWebElement(locatorType, itemsLocator);
         Actions actions = new Actions(driver);
         actions.contextClick(Allitems).perform();
         common.clickElement(locatorType, masterLocator);
-    }
-
-    public void selectFromLookup(String master) {
-        List<WebElement> elementList = common.findWebElements("xpath", "//Table[@Name='Lookup']/*/*[contains(@Name,'Master Row')]");
-        System.out.println("Size :" + elementList.size());
-        for (WebElement i : elementList) {
-            System.out.println(i.getText());
-            if (i.getText().contains(master)) {
-                i.click();
-            }
-        }
     }
 
     public void saveMaster() throws InterruptedException {
@@ -92,7 +59,6 @@ public class Masters {
     public void closeMaster(String masterName) {
         common.clickElement("xpath", "//TabItem[@Name='" + masterName + "']/Button[@Name='Close']");
     }
-
 
     public void inputTextWithValidation(String locatorType, String locator, String inputText) {
         WebElement element = common.findWebElement(locatorType, locator);
