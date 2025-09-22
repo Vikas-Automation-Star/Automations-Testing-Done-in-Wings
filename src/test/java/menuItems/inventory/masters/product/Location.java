e7d3e2c645e665f14938dee42c0c090a905ca4e3
Madhuri M <madhurimatta@wingsinfo.net>
2025-09-22 17:50:53 +0530

    [Madhuri] excel difference files and sales price class

---
+++ src/test/java/menuItems/inventory/masters/product/Location.java
@@ -0,0 +1,35 @@
+package menuItems.inventory.masters.product;
+
+import com.wings.pages.AppLogin;
+import com.wings.pages.inventory.masters.product.Locations;
+import io.appium.java_client.windows.WindowsDriver;
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.io.IOException;
+
+public class Location {
+    WindowsDriver driver;
+    AppLogin appLogin = new AppLogin();
+    String file = "./src/main/resources/menuItems/inventory/masters/locations.json";
+
+    @BeforeTest
+    public void beforeTest() throws InterruptedException, IOException, ParseException {
+        driver = appLogin.launchSingleUserApp();
+        appLogin.singleUserLogin();
+    }
+
+    @Test
+    public void locationMaster() throws Exception, AWTException {
+        Locations locations = new Locations(driver, file);
+        locations.locationMasterCreation();
+    }
+
+    @AfterTest
+    public void afterTest() throws IOException {
+        appLogin.logout();
+    }
+}
---
+++ src/test/java/menuItems/inventory/masters/product/Product.java
@@ -0,0 +1,35 @@
+package menuItems.inventory.masters.product;
+
+import com.wings.pages.AppLogin;
+import com.wings.pages.inventory.masters.product.Products;
+import io.appium.java_client.windows.WindowsDriver;
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.io.IOException;
+
+public class Product {
+    WindowsDriver driver;
+    AppLogin appLogin = new AppLogin();
+    String file = "./src/main/resources/menuItems/inventory/masters/product.json";
+
+    @BeforeTest
+    public void beforeTest() throws InterruptedException, IOException, ParseException {
+        driver = appLogin.launchSingleUserApp();
+        appLogin.singleUserLogin();
+    }
+
+    @Test
+    public void productMaster() throws Exception, AWTException {
+        Products products = new Products(driver, file);
+        products.productMasterCreation();
+    }
+
+    @AfterTest
+    public void afterTest() throws IOException {
+        appLogin.logout();
+    }
+}
